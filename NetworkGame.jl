270c59f6577cb317eca0fcca1ce952de586449c5
elliott <elliottgreene1@gmail.com>
2021-06-02 12:33:15 -0400

    Added networkGameRound to NetworkGame.jl

--- NetworkGame.jl
+++ NetworkGame.jl
@@ -5,18 +5,25 @@ using LinearAlgebra
 # Functions from Python version
 ####################################
 
-function calcOj(j::Int64, prev_out, Wm, Wb::Vector{Float64})
+function calcOj(j::Int64, prev_out, Wm::Matrix{Float64}, Wb::Vector{Float64})
     x = dot(Wm[1:j,j][1:j], prev_out[1:j]) + Wb[j]
     return 1-(exp(x*-x))
 end
 
-function iterateNetwork(input::Float64, Wm, Wb::Vector{Float64})
+function iterateNetwork(input::Float64, Wm::Matrix{Float64}, Wb::Vector{Float64})
     prev_out = zeros(Float64, length(Wb))
     prev_out[1] = input
     for j in 2:length(Wb)
         prev_out[j] = calcOj(j, prev_out, Wm, Wb)
     end
     return prev_out
+end
+
+function networkGameRound(mutWm, mutWb, mutPrev, resWm, resWb, resPrev)
+    mutOut = iterateNetwork(resPrev, mutWm, mutWb)
+    resOut = iterateNetwork(mutPrev, resWm, resWb)
+    print(mutOut, resOut)
+    return [mutOut, resOut]
 end
 
 ##################
@@ -31,6 +38,8 @@ Wb = randn(Float64, nnet)
 #Random Test Matrix Values
 #################
 
-Wm = reshape([0.71824181,2.02987316,-0.42858626,0.6634413],2,2)
+Wm = transpose(reshape([0.71824181,2.02987316,-0.42858626,0.6634413],2,2))
 Wb = [-0.66332791,1.00430577]
-iterateNetwork(0.0, Wm, Wb)
+init = 0.1
+networkGameRound(Wm, Wb, init, Wm, Wb, init)
+
