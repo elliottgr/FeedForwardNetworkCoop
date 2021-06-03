6f4b2aff34454c12d9d7a7a28342789d0ae4b8fc
elliott <elliottgreene1@gmail.com>
2021-06-03 12:54:15 -0400

    added comments to functions

--- Julia/NetworkGame.jl
+++ Julia/NetworkGame.jl
@@ -2,39 +2,22 @@
 using LinearAlgebra
 
 ####################################
-# Functions from Python version
+# Structs
 ####################################
 
 mutable struct simulation_parameters
-    ########
-    ## Population Simulation Parameters
-    ########
-    #Maximum number of timesteps
     tmax::Int64
-    #number of replicates
     nreps::Int64
-    #population size
     N::Int64
-    #mutation probability per birth
     μ::Float64
-
-    ########
-    ## Game Parameters
-    ########
-    #number of rounds the game is played between individuals
     rounds::Int64
-
-    #scales the fitness payout of game rounds by this amount (payoff * scale)
     fitness_benefit_scale::Float64
-
-    #Parameters for fitness payoff of game
     b::Float64
     c::Float64
     d::Float64
     δ::Float64
 
 end
-
 
 mutable struct network
     Wm
@@ -43,10 +26,23 @@ mutable struct network
     CurrentOffer::Float64
 end
 
+####################################
+# Functions from Python version
+####################################
+
+
+##############################
+## Iterates a single layer of the Feed Forward network
+##############################
 function calcOj(j::Int64, prev_out, Wm, Wb::Vector{Float64})
     x = dot(Wm[1:j,j][1:j], prev_out[1:j]) + Wb[j]
     return 1-(exp(x*-x))
 end
+
+##############################
+## Calculates the total output of the network,
+## iterating over calcOj() for each layer
+##############################
 
 function iterateNetwork(input::Float64, Wm, Wb::Vector{Float64})
     prev_out = zeros(Float64, length(Wb))
@@ -57,13 +53,23 @@ function iterateNetwork(input::Float64, Wm, Wb::Vector{Float64})
     return prev_out
 end
 
+##############################
+## Iterates above functions over a pair of networks,
+## constitutes a single game round
+##############################
+
 function networkGameRound(mutNet, resNet)
     mutOut = last(iterateNetwork(resNet.CurrentOffer, mutNet.Wm, mutNet.Wb))
     resOut = last(iterateNetwork(mutNet.CurrentOffer, resNet.Wm, resNet.Wb))
     return [mutOut, resOut]
 end
 
-function repeatedNetworkGameHistory(parameters, mutNet, resNet)
+##############################
+## Plays multiple rounds of the network game, returns differing 
+## data types depending on whether a discount needs to be calculated
+##############################
+
+function repeatedNetworkGame(parameters, mutNet, resNet)
     mutNet.CurrentOffer = mutNet.InitialOffer
     resNet.CurrentOffer = mutNet.InitialOffer
     mutHist = zeros(parameters.rounds)
@@ -82,26 +88,33 @@ end
 
 
 ####################################
-## calulate resident-mutant fitness matrix from contributions throughout the game history,
-## discounted at rate delta going from present to the past in the repeated cooperative
-## investment game
+## calulate resident-mutant fitness matrix from contributions throughout the game history. If
+## discount is availible, applied at rate delta going from present to the past in the 
+## repeated cooperative investment game
 ##
 ## fitness = 1 + b * (partner) - c * (self) + d * (self) * (partner)
 ####################################
 
 function fitnessOutcome(parameters::simulation_parameters,mutNet,resNet)
 
+    ############################################################
+    ## with discountretrieves the full history and passes it to the
+    ##  discount formula before returning final fitness value
+    ############################################################
     if parameters.δ >= 0.0
-        rmOut, mrOut = repeatedNetworkGameHistory(parameters,mutNet,resNet)
+        rmOut, mrOut = repeatedNetworkGame(parameters,mutNet,resNet)
         discount = exp.(-parameters.δ.*(parameters.rounds.-1 .-range(1,parameters.rounds, step = 1)))
         td = sum(discount)
         wmr = max(0, (1 + dot((parameters.b * rmOut - parameters.c * mrOut + parameters.d * rmOut.*mrOut), discount) * parameters.fitness_benefit_scale))
         wrm = max(0, (1 + dot((parameters.b * mrOut - parameters.c * rmOut + parameters.d * rmOut.*mrOut), discount)*parameters.fitness_benefit_scale))
         return [[wmr, wrm], [dot(rmOut, discount)/td, dot(mrOut, discount)/td]]
 
+    ############################################################
+    ## without discount, retrieves only the final value after all 
+    ## rounds played and returns it as w based on game parameters
+    ############################################################
     elseif parameters.δ < 0.0
         rmOut, mrOut = repeatedNetworkGameHistory(parameters, mutNet, resNet)
-        print(rmOut)
         wmr = max(0, (1 + ((parameters.b * rmOut - parameters.c * mrOut + parameters.d * rmOut.*mrOut)*parameters.fitness_benefit_scale)))
         wrm = max(0, (1 + ((parameters.b * mrOut - parameters.c * rmOut + parameters.d * rmOut.*mrOut)*parameters.fitness_benefit_scale)))
         return [[wmr, wrm], [rmOut, mrOut]]
@@ -109,9 +122,15 @@ function fitnessOutcome(parameters::simulation_parameters,mutNet,resNet)
     end
 end
 
-##################
-# Parameters
-##################
+###################
+# Simulation Loop #
+###################
+function simulation(parameters::simulation_parameters, initNetwork::network)
+
+end
+###################
+#   Parameters    #
+###################
 
 using ArgParse
 arg_parse_settings = ArgParseSettings()
@@ -180,11 +199,11 @@ parsed_args = parse_args(ARGS, arg_parse_settings)
 
 
 nnet = 2
-Wm = transpose(reshape([0.71824181,2.02987316,-0.42858626,0.6634413],2,2))
-Wb = [-0.66332791,1.00430577]
+initWm = transpose(reshape([0.71824181,2.02987316,-0.42858626,0.6634413],2,2))
+initWb = [-0.66332791,1.00430577]
 init = 0.1
-a = network(Wm, Wb, init, init)
-params = simulation_parameters(parsed_args["tmax"], parsed_args["nreps"], parsed_args["N"], parsed_args["mu"],
+InitialNetwork = network(Wm, Wb, init, init)
+parameters = simulation_parameters(parsed_args["tmax"], parsed_args["nreps"], parsed_args["N"], parsed_args["mu"],
                                 parsed_args["rounds"], parsed_args["fitness_benefit_scale"], parsed_args["b"], 
                                 parsed_args["c"], parsed_args["d"], parsed_args["delta"])
 
