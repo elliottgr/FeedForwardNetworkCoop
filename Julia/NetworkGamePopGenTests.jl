9355e150520823147f9d593548470c787ac33342
elliott <elliottgreene1@gmail.com>
2021-06-23 14:12:25 -0400

    Removing old code

--- Julia/NetworkGamePopGenTests.jl
+++ Julia/NetworkGamePopGenTests.jl
@@ -8,85 +8,87 @@
 
 using Distributed
 
-addprocs(4, topology=:master_worker, exeflags="--project=$(Base.active_project())")
-@everywhere using ArgParse, JLD2
-@everywhere begin
-    #instantiating environment
-    using Pkg; Pkg.instantiate()
-    Pkg.activate(@__DIR__)
-    #loading dependencies
-    using ArgParse, JLD2
+function call_workers()
+    addprocs(4, topology=:master_worker, exeflags="--project=$(Base.active_project())")
+    @everywhere using ArgParse, JLD2
+    @everywhere begin
+        #instantiating environment
+        using Pkg; Pkg.instantiate()
+        Pkg.activate(@__DIR__)
+        #loading dependencies
+        using ArgParse, JLD2
 
 
-    include("NetworkGameFuncs.jl")
-    function parameter_output_map(parameters::simulation_parameters)
-        replicate_parameters = copy(parameters)
-        return simulation(population_construction(replicate_parameters))
-    end
-
-    function RunReplicates(parameters::simulation_parameters)
-        ## generation of initial network
-
-        # rep_outputs = repeat([parameters], parameters.nreps)
-        # for rep in 1:parameters.nreps
-        #     init_pop = population_construction(parameters)
-        #     rep_outputs[rep] = simulation(init_pop)
-        # end
-        # pmap(x->parameter_output_map(x) : x, rep_output)
-        rep_outputs = pmap(parameter_output_map, repeat([parameters], parameters.nreps))
-        nreps = length(rep_outputs)
-        print("$nreps replicates completed in: ")
-        return rep_outputs
-    end
-
-    ###################
-    #      main       #
-    ###################
-    function main()
-
-        ##########
-        ## Arg Parsing
-        ##########
-
- 
-        
-        parameters = initial_arg_parsing()
-
-        ## setting iterator of population frequency
-        ps = collect(0.0:parameters.init_freq_resolution:1.0)
-        n_workers = nworkers()
-        print("Starting replicates with $n_workers processes", "\n")
-
-        ## initializing output array
-        sim_outputs = Vector(undef, 0)
-
-        for (p, q) in zip(ps, reverse(ps))
-
-            print("p = ", p, "\n")
-
-            ## creating savable copy of the parameters
-
+        include("NetworkGameFuncs.jl")
+        function parameter_output_map(parameters::simulation_parameters)
             replicate_parameters = copy(parameters)
-            replicate_parameters.init_freqs = [p, q] 
-            # ###################
-            # # Simulation call #
-            # ###################
-            @time current_reps = RunReplicates(replicate_parameters)
-
-            push!(sim_outputs, current_reps)
-            
-        ## end of init_freq iteration loop
+            return simulation(population_construction(replicate_parameters))
         end
 
-    jldsave(parameters.filename; sim_outputs)
-    ###################
-    #   Data Output   #     
-    ###################
+        function RunReplicates(parameters::simulation_parameters)
+            ## generation of initial network
 
-    # sim data will be stored in previous section and saved to disk here. 
-    print("Simulation done in ")
+            # rep_outputs = repeat([parameters], parameters.nreps)
+            # for rep in 1:parameters.nreps
+            #     init_pop = population_construction(parameters)
+            #     rep_outputs[rep] = simulation(init_pop)
+            # end
+            # pmap(x->parameter_output_map(x) : x, rep_output)
+            rep_outputs = pmap(parameter_output_map, repeat([parameters], parameters.nreps))
+            nreps = length(rep_outputs)
+            print("$nreps replicates completed in: ")
+            return rep_outputs
+        end
+
+        ###################
+        #      main       #
+        ###################
+        function main()
+
+            ##########
+            ## Arg Parsing
+            ##########
+
+    
+            
+            parameters = initial_arg_parsing()
+
+            ## setting iterator of population frequency
+            ps = collect(0.0:parameters.init_freq_resolution:1.0)
+            n_workers = nworkers()
+            print("Starting replicates with $n_workers processes", "\n")
+
+            ## initializing output array
+            sim_outputs = Vector(undef, 0)
+
+            for (p, q) in zip(ps, reverse(ps))
+
+                print("p = ", p, "\n")
+
+                ## creating savable copy of the parameters
+
+                replicate_parameters = copy(parameters)
+                replicate_parameters.init_freqs = [p, q] 
+                # ###################
+                # # Simulation call #
+                # ###################
+                @time current_reps = RunReplicates(replicate_parameters)
+
+                push!(sim_outputs, current_reps)
+                
+            ## end of init_freq iteration loop
+            end
+
+        jldsave(parameters.filename; sim_outputs)
+        ###################
+        #   Data Output   #     
+        ###################
+
+        # sim data will be stored in previous section and saved to disk here. 
+        print("Simulation done in ")
+        end
+
     end
-
 end
 @time main()
 
