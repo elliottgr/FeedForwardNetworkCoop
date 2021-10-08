5f90a0bb46d00ccf52ecc07f746183e0a38e8ae3
elliott <elliottgreene1@gmail.com>
2021-10-08 10:45:15 -0400

    Simplest possible NN figure

--- Python/Network_fig.py
+++ Python/Network_fig.py
@@ -6,5 +6,56 @@ Created on Fri Oct  8 07:17:34 2021
 """
 
 import networkx as nx
+import matplotlib.pyplot as plt
+import graphviz
 
-G = nx.digraph()
+
+## Graph Params
+
+hidden_layers = 3
+layer_nodes = 1
+
+## Initialize Nodes
+
+dot = graphviz.Digraph(comment = "A Neural Network")
+
+s = graphviz.Digraph('sub')
+s.attr(rank= 'same')
+
+
+for n in range(hidden_layers):
+    dot.node("Gene "+ str(n))
+
+
+
+
+## Initialize Edges 
+
+for n in range(hidden_layers):
+    s.edge("Input", "Gene " + str(n))
+    s.edge("Gene " +str(n), "Cooperation")
+    for n2 in range(hidden_layers):
+        if n2 > n:
+            s.edge("Gene "+ str(n), "Gene " +str(n2))
+
+s.edge("Input", "Cooperation")
+
+
+
+## Render Properties
+
+dot.subgraph(s)
+dot.edge_attr.update(arrowhead='vee')
+
+## Render Call
+dot.graph_attr['rankdir'] = 'LR'
+# dot.graph_attr['constraint'] = "false"
+
+dot.render("output.gv", view =True, format = "png")
+
+
+
+
+# %%
+
+# %%
