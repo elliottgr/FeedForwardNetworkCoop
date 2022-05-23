ed746621c0991a720c08ca8efdb7d15bfb484e95
elliott <elliottgreene1@gmail.com>
2022-05-23 11:43:18 -0400

    added new activation function for unbounded linear

--- Julia/ActivationFuncs.jl
+++ Julia/ActivationFuncs.jl
@@ -1,5 +1,9 @@
 ## A selection of functions passable to calcOj in the NetworkGameFuncs.jl game loop
 
+function linear_unbounded(x)
+    return x
+end
+
 function linear(x)
     return minimum([1, maximum([x, 0])])
 end
