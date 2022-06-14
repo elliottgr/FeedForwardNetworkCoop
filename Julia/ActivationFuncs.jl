08575db762d3fdee790c5404368c7509f3642629
elliott <elliottgreene1@gmail.com>
2022-06-14 10:36:40 -0400

    added new activation functions

--- Julia/ActivationFuncs.jl
+++ Julia/ActivationFuncs.jl
@@ -1,14 +1,26 @@
 ## A selection of functions passable to calcOj in the NetworkGameFuncs.jl game loop
 
-function linear_unbounded(x)
-    return x
+function heaviside(x)
+    if x > 0
+        return 1
+    elseif x <= 0
+        return 0
+    end
+end
+
+function ReLU(x)
+    return maximum(0, x)
 end
 
 function linear(x)
+    return x
+end
+
+function bounded_linear(x)
     return minimum([1, maximum([x, 0])])
 end
 
-##JVC's exponential
+##JVC's exponential AKA Logistic
 function jvc_exp(x)
     return (1/(1+exp(-x)))
 end
